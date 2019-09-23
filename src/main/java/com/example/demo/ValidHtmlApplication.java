4efd94ac0658b79f3d0ffdd714169f98c34cd611
jnfrle <jennifer.lee1@montgomerycollege.edu>
2019-09-23 10:35:21 -0400

    initial commit

---
+++ src/main/java/com/example/demo/ValidHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidHtmlApplication.class, args);
+    }
+
+}
